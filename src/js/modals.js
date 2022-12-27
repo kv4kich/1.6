3d4ff7f357d4940773cb4d066a5ce17a53b8c10c
Ilia Kozlov <iliakozlov778@gmail.com>
2022-12-28 02:23:59 +0700

    починил модалки, много мелких правок

--- src/js/modals.js
+++ src/js/modals.js
@@ -7,23 +7,38 @@ document.addEventListener('DOMContentLoaded', function() {
       body = document.querySelector('.body');
 
 
+
+
+
   modalButtons.forEach(function(item){
 
      item.addEventListener('click', function(e) {
 
+
+        var activeModals = document.querySelectorAll('.active')  
+
+        activeModals.forEach((activeModal) => { //закрываем все открытые модалки
+            activeModal.classList.remove('active');
+            body.classList.remove('body-scrolllock'); 
+        })
+
+        
 
         e.preventDefault();
 
 
         var modalId = this.getAttribute('data-modal'),
             modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
+            
 
 
 
         modalElem.classList.add('active');
         overlay.classList.add('active');
         body.classList.add('body-scrolllock');
-     }); // end click
+        document.querySelector('.autofocus').focus(); //открываем нажатую модалку
+
+    }); // end click
 
   }); // end foreach
 
