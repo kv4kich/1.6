52fb4399ff814e042be44f46d5a1a3dfec62a43a
Ilia Kozlov <iliakozlov778@gmail.com>
2022-12-28 07:37:50 +0700

    сделал scrolllock для safari

--- src/js/modals.js
+++ src/js/modals.js
@@ -1,85 +1,71 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-
+document.addEventListener('DOMContentLoaded', function () {
   var modalButtons = document.querySelectorAll('.js-open-modal'),
-      overlay      = document.querySelector('.js-overlay-modal'),
-      closeButtons = document.querySelectorAll('.js-modal-close');
-      body = document.querySelector('.body');
+    overlay = document.querySelector('.js-overlay-modal'),
+    closeButtons = document.querySelectorAll('.js-modal-close')
+  scrolllocks = document.querySelectorAll('.scrolllock')
 
+  modalButtons.forEach(function (item) {
+    item.addEventListener('click', function (e) {
+      var activeModals = document.querySelectorAll('.active')
 
-
-
-
-  modalButtons.forEach(function(item){
-
-     item.addEventListener('click', function(e) {
-
-
-        var activeModals = document.querySelectorAll('.active')  
-
-        activeModals.forEach((activeModal) => { //закрываем все открытые модалки
-            activeModal.classList.remove('active');
-            body.classList.remove('body-scrolllock'); 
+      activeModals.forEach((activeModal) => {
+        //закрываем все открытые модалки
+        activeModal.classList.remove('active')
+        scrolllocks.forEach(function (scrolllock) {
+          scrolllock.classList.remove('scrolllock-active')
         })
+        body.classList.remove('body-scrolllock')
+      })
 
-        
+      e.preventDefault()
 
-        e.preventDefault();
+      var modalId = this.getAttribute('data-modal'),
+        modalElem = document.querySelector(
+          '.modal[data-modal="' + modalId + '"]'
+        )
 
+      modalElem.classList.add('active')
+      overlay.classList.add('active')
+      scrolllocks.forEach(function (scrolllock) {
+        scrolllock.classList.add('scrolllock-active')
+      })
+      document.querySelector('.autofocus').focus() //открываем нажатую модалку
+    }) // end click
+  }) // end foreach
 
-        var modalId = this.getAttribute('data-modal'),
-            modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
-            
+  closeButtons.forEach(function (item) {
+    item.addEventListener('click', function (e) {
+      var parentModal = this.closest('.modal')
 
+      parentModal.classList.remove('active')
+      overlay.classList.remove('active')
+      scrolllocks.forEach(function (scrolllock) {
+        scrolllock.classList.remove('scrolllock-active')
+      })
+    })
+  }) // end foreach
 
+  document.body.addEventListener(
+    'keyup',
+    function (e) {
+      var key = e.keyCode
 
-        modalElem.classList.add('active');
-        overlay.classList.add('active');
-        body.classList.add('body-scrolllock');
-        document.querySelector('.autofocus').focus(); //открываем нажатую модалку
+      if (key == 27) {
+        document.querySelector('.modal.active').classList.remove('active')
+        document.querySelector('.overlay').classList.remove('active')
+        scrolllocks.forEach(function (scrolllock) {
+          scrolllock.classList.remove('scrolllock-active')
+        })
+      }
+    },
+    false
+  )
 
-    }); // end click
-
-  }); // end foreach
-
-
-  closeButtons.forEach(function(item){
-
-     item.addEventListener('click', function(e) {
-        var parentModal = this.closest('.modal');
-
-        parentModal.classList.remove('active');
-        overlay.classList.remove('active');
-        body.classList.remove('body-scrolllock');
-     });
-
-  }); // end foreach
-
-
-   document.body.addEventListener('keyup', function (e) {
-       var key = e.keyCode;
-
-       if (key == 27) {
-
-           document.querySelector('.modal.active').classList.remove('active');
-           document.querySelector('.overlay').classList.remove('active');
-           body.classList.remove('body-scrolllock');
-       };
-   }, false);
-
-
-   overlay.addEventListener('click', function() {
-       document.querySelector('.modal.active').classList.remove('active');
-       this.classList.remove('active');
-       body.classList.remove('body-scrolllock');
-   });
-
-
-
-
-}); // end ready
-
-
-
-
-
+  overlay.addEventListener('click', function () {
+    document.querySelector('.modal.active').classList.remove('active')
+    this.classList.remove('active')
+    scrolllocks.forEach(function (scrolllock) {
+      scrolllock.classList.remove('scrolllock-active')
+    })
+  })
+}) // end ready
